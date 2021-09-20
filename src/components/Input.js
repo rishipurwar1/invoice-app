eab23649403ad9958af42c357bf209c0c3ecc783
rishipurwar1 <rishipurwar007@gmail.com>
2021-09-20 17:18:32 +0530

    remove required

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ import { useFormContext } from "react-hook-form";
 const Input = ({ inputName, readOnly }) => {
     const { register } = useFormContext();
     return (
-        <input {...(readOnly && { disabled: true, readOnly, value: 0 })} type="text" id={inputName} className="w-full bg-primaryOne p-3 rounded-md shadow-md border border-borderOne focus:outline-none focus:border-secondaryOne transition text-white font-bold text-xs" {...register(inputName, { required: true })} />
+        <input {...(readOnly && { disabled: true, readOnly, value: 0 })} type="text" id={inputName} className="w-full bg-primaryOne p-3 rounded-md shadow-md border border-borderOne focus:outline-none focus:border-secondaryOne transition text-white font-bold text-xs" {...register(inputName)} />
     )
 }
 
