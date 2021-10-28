cd1096f5a355739934b00c343f25ee005c675815
Rishi Purwar <57035408+rishipurwar1@users.noreply.github.com>
2021-10-28 11:40:24 +0530

    add skeleton card

--- src/components/invoice/Invoices.js
+++ src/components/invoice/Invoices.js
@@ -2,15 +2,17 @@ import React from "react";
 import InvoiceCard from "./InvoiceCard";
 // import data from '../data.json'
 import { useSelector } from "react-redux";
+import SkeletonCard from "../skeletons/SkeletonCard";
 
 const Invoices = () => {
   const invoices = useSelector((state) => state.invoices);
   return (
     <div className="text-white w-full">
-      {invoices &&
-        invoices.map((cardData) => (
-          <InvoiceCard data={cardData} key={cardData._id} />
-        ))}
+      {invoices.length > 0
+        ? invoices.map((cardData) => (
+            <InvoiceCard data={cardData} key={cardData._id} />
+          ))
+        : [1, 2, 3, 4, 5, 6].map((n) => <SkeletonCard key={n} />)}
     </div>
   );
 };
